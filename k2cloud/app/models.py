de723c75a23b3c342082739aa6cc031034451bd6
Sandesh Jain <sandeshjain2507@gmail.com>
2022-06-21 16:59:48 +0530

    Contact View Updated and Added To Admin

--- k2cloud/app/models.py
+++ k2cloud/app/models.py
@@ -1,9 +1,13 @@
 from importlib.metadata import requires
 from django.db import models
-
+import datetime
 # Create your models here.
 class Contact(models.Model):
     Name = models.CharField( null = False, max_length=100)
     Contact = models.CharField(null = False, max_length=20)
     Email = models.EmailField(null = False)
     Message = models.CharField(null = False, max_length=1000)
+    postedOn =models.DateTimeField(default=datetime.datetime.now(), null = False )
+
+    def __str__(self):
+        return self.Name + " - " + str(self.postedOn.date())
